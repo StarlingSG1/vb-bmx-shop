b640fd72234abbf4a297011ae72d44233da5b849
Jérémie Barrière <barriere.jeremie@gmail.com>
2022-09-20 23:29:36 +0200

    paiement resultat

---
+++ pages/payement/canceled.js
@@ -0,0 +1,15 @@
+import Head from "next/head";
+import { Template } from "../../components/molecules";
+
+export default function Canceled(){
+    return (
+        <>
+        <Head>
+            <title>Echec paiement</title>
+        </Head>
+        <Template title="Echec de la commande">
+
+        </Template>
+        </>
+        )
+}
---
+++ pages/payement/success.js
@@ -0,0 +1,15 @@
+import Head from "next/head";
+import { Template } from "../../components/molecules";
+
+export default function Success(){
+    return (
+        <>
+        <Head>
+            <title>Succès paiement</title>
+        </Head>
+        <Template title="Paiement réussi">
+
+        </Template>
+        </>
+        )
+}
