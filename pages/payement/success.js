d8f78ad21d0df0b937123f620b5b19ea71c84f66
Jérémie Barrière <barriere.jeremie@gmail.com>
2022-10-11 21:18:50 +0200

    paiement page

--- pages/payement/success.js
+++ pages/payement/success.js
@@ -13,7 +13,12 @@ export default function Success(){
             <title>Succès paiement</title>
         </Head>
         <Template title="Paiement réussi">
-
+            <div className="shadow-card h-56 w-[800px] bg-white rounded-lg m-auto px-10 py-5">
+                <p className={"font-lato text-center text-intermediate font-medium mb-5"}>Merci pour votre commande</p>
+                <p className="font-lato text-bigger text-blue mb-4">Votre commande a bien été enregistré !</p>
+                <p className="font-lato text-bigger text-blue mb-4">Vous pouvez retrouver votre commande dans l'onglet <strong>Mon compte.</strong></p>
+                <p className={"font-lato text-blue text-bigger"}>Un email avec notre <strong>numéro de commande</strong> vous a également été envoyé.</p>
+            </div>
         </Template>
         </>
         )
